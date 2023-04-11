bdaa2924d42d6707a400afa8df27f18344adb100
Francisco Nogueira <francisco.nogueira92@gmail.com>
2023-04-11 20:08:31 +0100

    app update

--- scripts/app.py
+++ scripts/app.py
@@ -23,6 +23,69 @@ def index():
     return "Hello World"
 
 
+@app.route("/prediction", methods=['POST','OPTIONS'])
+def predict():        
+    """
+    call the prediction function you created in Step 3
+    
+    Returns:
+        preds_json: model predictions
+    """
+    filepath = request.get_json()['filepath']
+
+    data = pd.read_csv(filepath)
+    features = ["lastmonth_activity", "lastyear_activity", "number_of_employees"]
+    X = data[features]
+    
+
+    preds = model_predictions(X)
+    preds_json = jsonify(preds.tolist())
+    
+    return preds_json
+
+
+#######################Scoring Endpoint
+@app.route("/scoring", methods=['GET','OPTIONS'])
+def calculate_score():        
+    """
+    Scoring endpoint that runs the script scoring.py and
+    gets the score of the deployed model
+    
+    Returns:
+        str: model f1 score
+    """
+    
+    f1_score = subprocess.run(['python', 'scoring.py'],capture_output=True)
+    
+    return f1_score
+
+
+#######################Summary Statistics Endpoint
+@app.route("/summarystats", methods=['GET','OPTIONS'])
+def stats():        
+    """
+    Calls summary statistics functions from diagnostics module
+    """
+    return jsonify(dataframe_summary())
+
+
+#######################Diagnostics Endpoint
+@app.route("/diagnostics", methods=['GET','OPTIONS'])
+def diagnostics():        
+    
+    #Get functions results
+    missing = get_missings()
+    time = execution_time()
+    outdated = outdated_packages_list()
+
+    results = {
+        'missing_percentage': missing,
+        'execution_time': time,
+        'outdated_packages': outdated
+    }
+
+    return jsonify(results)
+
 #######################Prediction Endpoint
 if __name__ == "__main__":    
     app.run(host='127.0.0.1', port=5000, debug=True, threaded=True)
